7aa8a2b1ca707c9a57f9953500122afc35ccd446
Ric <rachim27reuel@gmail.com>
2026-03-26 23:44:14 +0530

    Fixing some minor issues

--- TLSWeb/src/login.jsx
+++ TLSWeb/src/login.jsx
@@ -15,7 +15,7 @@ export function Login() {
     const data = { username, password };
 
     try {
-      const res = await fetch(`${API_URL}/admin`, {
+      const res = await fetch(`${API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
