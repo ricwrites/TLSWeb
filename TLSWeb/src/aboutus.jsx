e73b0e83534299da70fcec1b4b91ddfb6542ebc8
Ric <rachim27reuel@gmail.com>
2026-04-26 16:08:17 +0530

    Updating the About Us page

--- TLSWeb/src/aboutus.jsx
+++ TLSWeb/src/aboutus.jsx
@@ -10,7 +10,7 @@ export const AboutUs = () => {
       </p>
       <p>Founded in 2022 by Tyrone D'Brass, The Learning Sanctuary aims to provide quality education to children living in Tura, Meghalaya. The students are provided with a holistic pathway to achieve their learning in the classroom and outside. </p>
     <p> Located near the MBOSE headquarters, the school's campus has a cascading row of school rooms and a ground for games. Alongside their curricular adventures, children are also exposed to enriching extra-curricular activities to become well-rounded, considerate and intelligent young citizens of the world. With classrooms equipped with state-of-the-art interactive whiteboards and a roster of educators keen to facilitate the students' learning, the school is an intellectually stimulating home away from home for its students.</p> 
-<p> The school currently offers seats for students ranging from Nursery to Class IX. To enquire about admissions for your child(ren), please call or visit our campus Monday to Friday, 09:00 to 14:00 and Saturdays, 10:00 to 12:30. </p>
+<p> The school currently offers seats for students ranging from Nursery to Class IX. To enquire about admissions for your child(ren), please call us on +918794961440 or visit our campus Monday to Friday, 09:00 to 14:00 and Saturdays, 10:00 to 12:30. </p>
 
       <p>
         {/* Add principal's notes here */}
